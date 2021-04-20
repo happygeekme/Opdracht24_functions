7c2f1bd3164fe808cacafc786ba912c7dd1fe21d
Jitske <80577121+happygeekme@users.noreply.github.com>
2021-04-20 17:32:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const painter = function (whatWall, color) {
+    console.log("The " + whatWall + " has been painted " + color)
+};
+
+// painter("black");
+// painter("orange");
+
+// painter();
+//the wall has been painted undefined
+
+// painter("northwall", "green")
+
+painter("yellow", "southwall")
